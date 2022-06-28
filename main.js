08aff185683e9b91c6ce3c2e1dd18200fe7c441e
github_Jeptooleonida <leonida.jeptoo@student.moringaschool.comgit config --global user.name Jeptooleonidagit config --global user.email leonida.jeptoo@student.moringaschool.com>
2022-06-28 03:27:27 +0300

    commit tree 9811ff93af9e34e1ea1e1d5e2e00cf25f3a7669b

--- main.js
+++ main.js
@@ -4,6 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// create a new variable to select the hidden class in html file
+const displayMsg = document.getElementById('modal')
+const errorMsg = document.getElementById('modal-message')
+const likeHearts = document.querySelectorAll(".like-glyph");
+
+// function for clicking an empty heart
+likeHearts.forEach(element => {
+  element.addEventListener('click', () => {
+  mimicServerCall()
+  .then(() => {
+    //(element.innerHTML === EMPTY_HEART)?FULL_HEART : EMPTY_HEART
+    if (element.textContent === EMPTY_HEART) {
+       element.textContent = FULL_HEART
+       element.className = 'activated-heart'
+    } else {
+      element.textContent = EMPTY_HEART
+      elememt.className = 'activated-heart'
+    }
+  })
+  .catch(error => {
+    displayMsg.classList.remove('hidden')
+    errorMsg.textContent = error
+    setTimeout(() => {
+      displayMsg.classList.add('hidden')
+      errorMsg.textContent = ''
+    }, 2000)
+  })
+  })
+  })
+
+
+
 
 
 
